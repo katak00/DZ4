13820bad66655c69db1518a0d604d6bd54d682d7
katak0 <tolhiev@gmail.com>
2023-03-14 15:39:39 +0500

    remake project

--- main.cpp
+++ main.cpp
@@ -1,14 +1,47 @@
 #include <iostream>
 #include <limits>
-#include "variables.h"
-#include "read_data.cpp"
-#include "write_data.cpp"
-#include "clear_data.cpp"
+#include"variables.h"
+
 
 int main()
 {
-	read_data();
-	write_data();
-	clear_data();
+	Min_statistics[0] = new Min{};
+	Max_statistics[0] = new Max{};
+	Mean_statistics[0] = new Mean{};
+	Std_statistics[0] = new Std{};
+
+	while (std::cin >> val)
+	{
+		for (size_t i = 0; i < statistics_count; ++i)
+		{
+			Min_statistics[i]->update(val);
+			Max_statistics[i]->update(val);
+			Mean_statistics[i]->update(val);
+			Std_statistics[i]->update(val);
+		}
+	}
+	// Handle invalid input data
+	if (!std::cin.eof() && !std::cin.good()) {
+		std::cerr << "Invalid input data\n";
+		return 1;
+	}
+
+	// Print results if any
+	for (size_t i = 0; i < statistics_count; ++i) {
+		std::cout << Min_statistics[i]->name() << " = " << Min_statistics[i]->eval() << std::endl;
+		std::cout << Max_statistics[i]->name() << " = " << Max_statistics[i]->eval() << std::endl;
+		std::cout << Mean_statistics[i]->name() << " = " << Mean_statistics[i]->eval() << std::endl;
+		std::cout << Std_statistics[i]->name() << " = " << Std_statistics[i]->eval() << std::endl;
+	}
+
+	
+	for (size_t i = 0; i < statistics_count; ++i) {
+		delete Min_statistics[i];
+		delete Max_statistics[i];
+		delete Mean_statistics[i];
+		delete Std_statistics[i];
+	}
+
+
 	return 0;
 }
